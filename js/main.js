db70649194f980aa9c032ba715c77c9eea333d08
Juliya <prakhova_yuliya@ukr.net>
2020-04-12 23:40:37 +0300

    new part

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$('#carousel1').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        750:{
+            items:4
+        },
+        1000:{
+            items:5
+        }
+    }
+})
